7de35db37c62d104efd482f92952ca65fca0eb01
Niko Ugarov <xxkkoo@gmail.com>
2021-02-01 18:33:51 +0100

    more detailed test

--- src/test/scala/xko/elevators/Spec.scala
+++ src/test/scala/xko/elevators/Spec.scala
@@ -13,8 +13,16 @@ class Spec extends AnyFlatSpec with Matchers {
   }
 
   it should "collect way up" in {
-    val before = ControlSystem(0).pickUp(1, 1).pickUp(2, 1).pickUp(4, 1)
-    val after = before.proceed(10)
-    after.elevators(0).floor should be(4)
+    val start = ControlSystem(0).pickUp(1, 1).pickUp(2, 1).pickUp(4, 1)
+    val steps = Iterator.iterate(start)(_.proceed).take(12).toIndexedSeq
+    steps(1).elevators(0).floor should be(1)
+    steps(1).elevators(0).isStopped should be(true)
+    steps(5).elevators(0).floor should be(2)
+    steps(6).elevators(0).isStopped should be(true)
+    steps(7).elevators(0).isStopped should be(true)
+    steps(9).elevators(0).floor should be(3)
+    steps(9).elevators(0).isStopped should be (false)
+    steps(10).elevators(0).floor should be(4)
+    steps(11).isIdle should be (true)
   }
 }
