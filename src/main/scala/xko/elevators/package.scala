de98318e11255a2283ab7b0cf140fb88a179cb18
Niko Ugarov <xxkkoo@gmail.com>
2021-02-03 23:11:18 +0100

    scaladoc for the API

--- src/main/scala/xko/elevators/package.scala
+++ src/main/scala/xko/elevators/package.scala
@@ -3,9 +3,13 @@ package xko
 import scala.collection.immutable.SortedSet
 
 package object elevators {
+  /** State of an elevator at particular step */
   trait Elevator {
+    /** Current floor */
     def floor: Int
+    /** Current direction:  1 for UP, -1 for DOWN, 0 means elevator is idle  */
     def dir: Int
+    /** True if elevator is currently stopped and passengers can board */
     def isStopped: Boolean
   }
 
@@ -13,18 +17,36 @@ package object elevators {
   val Down: Int = -1
   val Free: Int = 0
 
+  /** State of an elevator control system at particular step */
   trait ControlSystem {
     def elevators: IndexedSeq[Elevator]
+    /** Request a pick-up.
+     * @return new instance of ControlSystem with pick-up scheduled
+     */
     def pickUp(floor: Int, dir: Int): ControlSystem
+    /** Request a drop-off.
+     * @return new instance of ControlSystem with drop-off scheduled
+     */
     def dropOff(elevatorIndex: Int, floor: Int): ControlSystem
+
+    /** True if system currently have no scheduled requests and all elevators are idle */
     def isIdle: Boolean
+
+    /** Proceed to next step
+     * @return new instance of ControlSystem representing the state at the next step
+     */
     def proceed: ControlSystem
 
+    /** Repeatedly transform this ControlSystem with f() */
     def iterate(f: ControlSystem => ControlSystem):Iterator[ControlSystem] = Iterator.iterate(this)(f)
+    /** Skip number of steps
+     * @return state of the system after <code>steps</code> steps
+     */
     def proceed(steps: Int): ControlSystem= iterate(_.proceed).drop(steps).next()
   }
 
   object ControlSystem {
+    /** Creates an new idle ControlSystem with elevators at specified floors */
     def apply(floors: Int*): ControlSystem = Scheduler(floors.toIndexedSeq.map(lift),Set.empty,0)
   }
 
