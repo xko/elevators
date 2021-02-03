9ee5b306e19f5fb2b5b8e805d9db46bd5020eb32
Niko Ugarov <xxkkoo@gmail.com>
2021-02-03 12:58:47 +0100

    another test

--- src/test/scala/xko/elevators/Spec.scala
+++ src/test/scala/xko/elevators/Spec.scala
@@ -64,6 +64,11 @@ class Spec extends AnyFlatSpec with Matchers with Inside{
       val i = cs.elevators.indexWhere (e => e.floor == from && e.isStopped && e.dir * (to - from) >= 0)
       if (i >= 0) cs.dropOff(i, to) else cs
     }
+
+    def hijack(from: Int, to: Int): ControlSystem[Elevator] = {              // same, but ignore direction:
+      val i = cs.elevators.indexWhere(e => e.floor == from && e.isStopped)   // request up in the downward lift too
+      if (i >= 0) cs.dropOff(i, to) else cs
+    }
   }
 
   it should "prefer local pickups" in {
@@ -80,5 +85,16 @@ class Spec extends AnyFlatSpec with Matchers with Inside{
     val lowerStops = steps.filter(_.elevators(1).isStopped).map(_.elevators(1).floor).distinct
     upperStops should equal (IndexedSeq(100,126,130,125,81,80,75))
     lowerStops should equal (IndexedSeq(0,8,12,20,-2,-3))
+  }
+
+  it should "serve hijacks too, but later" in {
+    val start = ControlSystem(100,0).pickUp(125,Down).pickUp(126, Up).pickUp(81,Down)
+    val steps = Iterator.iterate(start) { cs =>
+      cs.ride(126, 130).hijack(126,122)
+        .ride(125, 80).ride(81, 75).ride(12, 10).ride(8, 20).ride(-2, -3)
+        .proceed
+    }.take(200).toIndexedSeq
+    val upperStops = steps.filter(_.elevators(0).isStopped).map(_.elevators(0).floor).distinct
+    upperStops should equal (IndexedSeq(100,126,130,125,122,81,80,75))
   }
 }
